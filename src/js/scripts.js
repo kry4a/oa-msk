7bb62c51b83724e44116d3e103b699c922c715e7
Nikita <nikita.kruchinkin@gmail.com>
2017-10-23 15:39:17 +0300

    Simple parallalx

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,5 +3,62 @@ $(function (){
   $('.nav__item--parent').click(function(e){
     console.log('clicked');
     $(this).toggleClass('nav__item--active');
+    return false;
   });
+
+  $('.link--readmore').click(function(e){
+    console.log('readmore');
+    var target = $(this).data('target');
+    $('#'+target).toggleClass('section__readmore--active');
+    return false;
+  });
+
+  $('.btn--orange').click(function(e){
+    $('.popup').show();
+    $('.popup__bg').show();
+  });
+
+
+  $('body').on('click','.popup__close',function(){
+    $('.popup').hide();
+    $('.popup__bg').hide();
+  });
+
+  $('.im--phone').mask('+7 (000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
+
+  $('.input--required').change(function(e){
+    $(this).removeClass('input--error');
+    $(this).removeClass('input--success');
+    
+    var val = $(this).val();
+    if ($(this).hasClass('im--phone')) {
+      if (val.length<18)
+        $(this).addClass('input--error');
+      else {
+        $(this).addClass('input--success');
+      }
+    } else {
+      if (val=='') {
+        $(this).addClass('input--error');
+      } else {
+        $(this).addClass('input--success');
+      } 
+    }
+    
+  });
+
+  
+  var parallax = document.querySelectorAll(".parallax"),
+      speed = 0.5;
+
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
+
+      var windowYOffset = window.pageYOffset,
+          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+      el.style.backgroundPosition = elBackgrounPos;
+
+    });
+  };
 });
