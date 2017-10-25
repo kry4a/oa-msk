a8bdb91b15b0479451cfaaa13b21fe948d29e799
Nikita <nikita.kruchinkin@gmail.com>
2017-10-25 18:48:11 +0300

    Mobile table and scrollbar (baron.js)

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -39,6 +39,12 @@ $(function (){
 
   $('.im--phone').mask('+7 (000) 000-00-00', {placeholder: "+7 (___) ___-__-__"});
 
+
+
+  $('.row__title').click(function(){
+    $(this).next('.row__body').toggleClass('active');
+  });
+  
   $('.input--required').change(function(e){
     $(this).removeClass('input--error');
     $(this).removeClass('input--success');
@@ -59,7 +65,25 @@ $(function (){
     }
     
   });
-
+  
+  baron({
+      root: '.baron',
+      scroller: '.baron__scroller',
+      bar: '.baron__bar',
+      scrollingCls: '_scrolling',
+      draggingCls: '_dragging'
+  }).fix({
+      elements: '.header__title',
+      outside: 'header__title_state_fixed',
+      before: 'header__title_position_top',
+      after: 'header__title_position_bottom',
+      clickable: true
+  }).controls({
+      // Element to be used as interactive track. Note: it could be different from 'track' param of baron.
+      track: '.baron__track',
+      forward: '.baron__down',
+      backward: '.baron__up'
+  });
   
   var parallax_1 = document.querySelectorAll(".parallax--1"),
       parallax_2 = document.querySelectorAll(".parallax--2"),
@@ -70,13 +94,13 @@ $(function (){
     [].slice.call(parallax_1).forEach(function(el,i){
 
       var windowYOffset = window.pageYOffset,
-          big_pos = "50% " + (windowYOffset * speed_big) + "px";
+          big_pos = "50% " + (windowYOffset * speed_big - 200) + "px";
           el.style.backgroundPosition = big_pos;
     });
     [].slice.call(parallax_2).forEach(function(el,i){
 
       var windowYOffset = window.pageYOffset,
-          small_pos = "50% " + (windowYOffset * speed_small) + "px";
+          small_pos = "50% " + (windowYOffset * speed_small - 200) + "px";
       el.style.backgroundPosition = small_pos;
     });
   };
