d45eba8810ae2a0954a60e21b1b6ad077b32a423
Nikita <nikita.kruchinkin@gmail.com>
2017-11-11 16:10:45 +0300

    Breadcrumbs component

---
+++ pixelaria/components/bitrix/breadcrumb/top/template.php
@@ -0,0 +1,30 @@
+<?
+if( !defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true ) die();
+
+if( empty( $arResult ) )
+	return "";
+
+$arItemLast = array();	
+foreach($arResult as $index => $arItem){
+	if($index && $arItem["LINK"] == $arItemLast["LINK"]){
+		unset($arResult[$index]);
+	}
+	else{
+		$arItemLast = $arItem;
+	}
+}
+
+$strReturn = '<ul class="breadcrumbs">';
+foreach($arResult as $arItem){
+	$title = htmlspecialcharsex($arItem["TITLE"]);
+	if( $arItem["LINK"] <> "" && $arItem['LINK'] != GetPagePath() && $arItem['LINK']."index.php" != GetPagePath())
+		$strReturn .= '<li class="breadcrumbs__item"><a class="breadcrumbs__link" href="'.$arItem["LINK"].'" title="'.$title.'">'.$title.'</a></li>';
+	else{
+		$strReturn .= '<li class="breadcrumbs__item">'.$title.'</li>';
+		break;
+	}
+}
+
+$strReturn .= '</ul>';
+return $strReturn;
+?>
